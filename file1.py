e2f17152fd91ac92e8bdcacb57a2da1a2b695640
Andrea T. Cantos <torresandreac12@gmail.com>
2024-07-05 12:01:58 +0800

    file.py has modify

--- file1.py
+++ file1.py
@@ -1,7 +1,3 @@
-def fibonacci_sequence(n):
- sequence = [0, 1]
- while len(sequence) < n:
- sequence.append(sequence[-1] + sequence[-2])
- return sequence
-result = fibonacci_sequence(10)
-print(f"Fibonacci sequence up to 10 numbers: {result}")
+x = 12
+y = "Hello, World!"
+print(x)
